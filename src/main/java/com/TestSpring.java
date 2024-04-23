047c7d7d225b42c763be74327ed5d6ae20ff67c9
Evgeny Bayandin <5087081@gmail.com>
2024-04-23 16:22:01 +0300

    Add Dependency Injection in new Spring Core project

--- src/main/java/com/TestSpring.java
+++ src/main/java/com/TestSpring.java
@@ -8,10 +8,10 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("musicBean", Music.class);
+//        Music music = context.getBean("musicBean", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
 
         context.close();
