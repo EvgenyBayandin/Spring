1598046af3de6830322ee4ac80d2dce71dbd2a3a
Evgeny Bayandin <5087081@gmail.com>
2024-04-23 16:38:32 +0300

    Add properties in applicationContext.xml in case Dependency Injection in new Spring Core project

--- src/main/java/com/MusicPlayer.java
+++ src/main/java/com/MusicPlayer.java
@@ -2,6 +2,24 @@ package com;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 
     // инверсия управления IoC
     public MusicPlayer(Music music) {
