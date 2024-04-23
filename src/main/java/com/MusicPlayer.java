5f3a04cc16708e415c0495aa80b65b076cffa78e
Evgeny Bayandin <5087081@gmail.com>
2024-04-23 16:32:20 +0300

    Update Dependency Injection in new Spring Core project

--- src/main/java/com/MusicPlayer.java
+++ src/main/java/com/MusicPlayer.java
@@ -8,6 +8,12 @@ public class MusicPlayer {
         this.music = music;
     }
 
+    public MusicPlayer() {
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
     public void playMusic() {
         System.out.println("Playing song: " + music.getSong());
     }
--- src/main/java/com/TestSpring.java
+++ src/main/java/com/TestSpring.java
@@ -8,9 +8,6 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-//        Music music = context.getBean("musicBean", Music.class);
-//        MusicPlayer musicPlayer = new MusicPlayer(music);
-
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
 
