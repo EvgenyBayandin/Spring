d2253ece7436cf983549125ba197338eabc71098
Evgeny Bayandin <5087081@gmail.com>
2024-04-24 15:41:59 +0300

    Add factory method in to bean in the file applicationContext.xml in case Dependency Injection in new Spring Core project

--- src/main/java/com/DanceMusic.java
+++ src/main/java/com/DanceMusic.java
@@ -2,6 +2,13 @@ package com;
 
 public class DanceMusic implements Music{
 
+    private DanceMusic(){}
+
+    //Factory Method/
+    public static DanceMusic getDanceMusic(){
+        return new DanceMusic();
+    }
+
     public void doMyInit(){
         System.out.println("DanceMusic doMyInit");
     }
