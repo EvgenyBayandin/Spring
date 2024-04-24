8c39488afde28fe8eeac0bf7eb767c9870af8c1c
Evgeny Bayandin <5087081@gmail.com>
2024-04-24 15:32:05 +0300

    Add scope="prototype" to bean in the file applicationContext.xml in case Dependency Injection in new Spring Core project

--- src/main/java/com/DanceMusic.java
+++ src/main/java/com/DanceMusic.java
@@ -2,8 +2,16 @@ package com;
 
 public class DanceMusic implements Music{
 
+    public void doMyInit(){
+        System.out.println("DanceMusic doMyInit");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("DanceMusic doMyDestroy");
+    }
+
     @Override
     public String getSong() {
-        return "Modern dance";
+        return "Modern dance song";
     }
 }
--- src/main/java/com/TestSpring.java
+++ src/main/java/com/TestSpring.java
@@ -8,21 +8,25 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        DanceMusic danceMusic = context.getBean("musicBean", DanceMusic.class);
 
-        boolean compare = firstMusicPlayer == secondMusicPlayer;
-        System.out.println(compare);
-        System.out.println(firstMusicPlayer.getName());
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getName());
-        System.out.println(secondMusicPlayer.getVolume());
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
+        System.out.println(danceMusic.getSong());
 
-        firstMusicPlayer.setVolume(10);
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean compare = firstMusicPlayer == secondMusicPlayer;
+//        System.out.println(compare);
+//        System.out.println(firstMusicPlayer.getName());
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getName());
+//        System.out.println(secondMusicPlayer.getVolume());
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        firstMusicPlayer.setVolume(10);
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
 
         context.close();
     }
