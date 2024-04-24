8b66a3d7263c90675aaf6f8ea0359d1f970498d8
Evgeny Bayandin <5087081@gmail.com>
2024-04-24 16:49:02 +0300

    Update @Autowired annotation and update the files in case Dependency Injection in new Spring Core project

--- src/main/java/com/MusicPlayer.java
+++ src/main/java/com/MusicPlayer.java
@@ -5,11 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class MusicPlayer {
     private Music music;
 
-    @Autowired
+
     public MusicPlayer(Music music) {
         this.music = music;
     }
 
+    @Autowired
+    public void setMusic(com.Music music) {
+        this.music = music;
+    }
 
     public void playMusic() {
         System.out.println("Playing song: " + music.getSong());
