61c9d05ad4c5da5fd7623f1b525f4ac2b834081f
Evgeny Bayandin <5087081@gmail.com>
2024-04-24 18:11:51 +0300

    Add @Scoupe and update files in case Dependency Injection in new Spring Core project

--- src/main/java/com/ClassicalMusic.java
+++ src/main/java/com/ClassicalMusic.java
@@ -1,8 +1,10 @@
 package com;
 
 import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Scope;
 
 @Component
+@Scope("prototype")
 public class ClassicalMusic implements Music {
 
     @Override
--- src/main/java/com/TestSpring.java
+++ src/main/java/com/TestSpring.java
@@ -28,6 +28,10 @@ public class TestSpring {
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
 
+        ClassicalMusic classicalMusic = context.getBean("classicalMusic", ClassicalMusic.class);
+        ClassicalMusic classicalMusic2 = context.getBean("classicalMusic", ClassicalMusic.class);
+
+        System.out.println(classicalMusic == classicalMusic2);
 
         context.close();
     }
